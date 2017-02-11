28a3470559816fc90df25c99d54ea1c6784c7c99
danishsaeed2 <danishsaeed2@gmail.com>
2017-02-11 11:58:29 +0530

    Update

--- health4kids/login.php
+++ health4kids/login.php
@@ -1,20 +1,25 @@
 <?php
-if(isset($_POST['schoolname'])&&isset($_POST['schoolemail'])&&isset($_POST['schoolmob'])&&isset($_POST['password'])){
-	 $schoolname=$_POST['schoolname'];
+if(isset($_POST['schoolemail'])&&isset($_POST['password'])){
 	 $schoolemail=$_POST['schoolemail'];
-	 $schoolmob=$_POST['schoolmob'];
 	 $password=$_POST['password'];
 	if(!@mysql_connect('localhost','root',''))
 		echo "not connected";
-	//echo $schoolemail;
 	$mysql_db='hack';
 	if(!@mysql_select_db($mysql_db))
 		echo "select dat";
-	$query="INSERT INTO `hack`.`dtu` VALUES ('$schoolname','$schoolemail','$schoolmob','$password','')";
+	$query="SELECT * FROM `hack`.`dtu` WHERE `Schoolemail` = '$schoolemail' and `Schoolpas` = '$password'";
 	if($queryrun=mysql_query($query))
 	{
-		echo "Registered succesfully";
-		header('Location: home.html');
+		$query_num_rows=mysql_num_rows($queryrun);
+		if($query_num_rows==0)
+			echo "please insert correct email and password";
+		else
+		{
+			while($query_row=mysql_fetch_assoc($queryrun)){
+				echo "welcome ".$query_row['Schoolname'];
+				echo "your current points are ".$query_row['schoolpoints'];
+			}
+		}
 	}
 	else
 		echo "fucvl";
